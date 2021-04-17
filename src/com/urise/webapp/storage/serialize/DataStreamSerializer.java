713799be311085bb01c467490911c6a71c46b515
pavel <natitshow@gmail.com>
2021-04-17 23:35:06 +0300

    HW10 help

--- src/com/urise/webapp/storage/serialize/DataStreamSerializer.java
+++ src/com/urise/webapp/storage/serialize/DataStreamSerializer.java
@@ -8,6 +8,7 @@ import java.util.List;
 import java.util.Map;
 
 public class DataStreamSerializer implements SerializeStrategy {
+
     @Override
     public void doWrite(Resume r, OutputStream os) throws IOException {
         try (DataOutputStream dos = new DataOutputStream(os)) {
@@ -19,6 +20,8 @@ public class DataStreamSerializer implements SerializeStrategy {
                 dos.writeUTF(entry.getKey().name());
                 dos.writeUTF(entry.getValue());
             }
+            Map<SectionType, AbstractSection> sections = r.getSections();
+            dos.writeInt(sections.size());
             for (Map.Entry<SectionType, AbstractSection> entry : r.getSections().entrySet()) {
                 SectionType type = entry.getKey();
                 AbstractSection section = entry.getValue();
@@ -27,12 +30,15 @@ public class DataStreamSerializer implements SerializeStrategy {
                     case OBJECTIVE:
                         dos.writeUTF(entry.getKey().name());
                         dos.writeUTF(((SingleLineSection) section).getText());
-                        continue;
+                        break;
                     case ACHIEVEMENT:
                     case QUALIFICATIONS:
                         dos.writeUTF(entry.getKey().name());
+                        List<String> list = new ArrayList<>();
                         for (String value : ((ListSection) section).getText()) {
                             dos.writeUTF(value);
+                            list.add(value);
+                            dos.writeInt(list.size());
                         }
                 }
             }
@@ -52,7 +58,8 @@ public class DataStreamSerializer implements SerializeStrategy {
                 String value = dis.readUTF();
                 resume.addContact(contactsType, value);
             }
-            for (int i = 0; i < 4; i++) {
+            int sizeSections = dis.readInt();
+            for (int i = 0; i < sizeSections; i++) {
                 SectionType sectionType = SectionType.valueOf(dis.readUTF());
                 switch (sectionType) {
                     case PERSONAL:
@@ -60,18 +67,19 @@ public class DataStreamSerializer implements SerializeStrategy {
                         resume.addSection(sectionType, new SingleLineSection(dis.readUTF()));
                     case ACHIEVEMENT:
                     case QUALIFICATIONS:
-                        resume.addSection(sectionType, new ListSection(readListSection(dis, dis.readUTF()));
+                        resume.addSection(sectionType, new ListSection(readListSection(dis)));
                 }
             }
             return resume;
         }
     }
 
-    private List<String> readListSection(DataInputStream dis, String readUTF) throws IOException {
+    private List<String> readListSection(DataInputStream dis) throws IOException {
         int size = dis.readInt();
+        System.out.println(size);
         List<String> list = new ArrayList<>(size);
         for (int i = 0; i < size; i++) {
-            list.add(readUTF);
+            list.add(dis.readUTF());
         }
         return list;
     }
