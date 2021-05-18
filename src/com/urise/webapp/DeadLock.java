367aff425cc9ea9243d345a73c49a5a3ab1c644c
Pavel Semenov <PNSemenov@sberbank.ru>
2021-05-18 07:27:04 +0300

    corrected Deadlock class

---
+++ src/com/urise/webapp/DeadLock.java
@@ -0,0 +1,28 @@
+package com.urise.webapp;
+
+
+class DeadLock {
+
+    public static void main(String[] args) {
+        String lock1 = "lock1";
+        String lock2 = "lock2";
+        DeadLock.deadLock(lock1, lock2);
+        DeadLock.deadLock(lock2, lock1);
+    }
+
+    public static void deadLock(Object lock1, Object lock2) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println(Thread.currentThread().getName() + " try to catch object of monitor " + lock1);
+                synchronized (lock1) {
+                    System.out.println(Thread.currentThread().getName() + " monitor of object " + lock1 + " was caught");
+                    System.out.println(Thread.currentThread().getName() + " try to catch object of monitor  " + lock2);
+                    synchronized (lock2) {
+                        System.out.println(Thread.currentThread().getName() + " monitor of object " + lock2 + " was caught");
+                    }
+                }
+            }
+        }).start();
+    }
+}
--- src/com/urise/webapp/MainConcurrency.java
+++ src/com/urise/webapp/MainConcurrency.java
@@ -1,8 +1,5 @@
 package com.urise.webapp;
 
-import com.urise.webapp.util.LazySingleton;
-import sun.awt.windows.ThemeReader;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -64,11 +61,6 @@ public class MainConcurrency {
         });
         System.out.println(mainConcurrency.counter);
 
-        String lock1 = "lock1";
-        String lock2 = "lock2";
-        DeadLock.deadLock(lock1, lock2);
-        DeadLock.deadLock(lock2, lock1);
-
     }
 
     private synchronized void inc() {
@@ -79,22 +71,5 @@ public class MainConcurrency {
 //                readFile
 //                ...
 //        }
-    }
-}
-class DeadLock{
-    public static void deadLock(Object lock1, Object lock2) {
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                System.out.println(Thread.currentThread().getName() + " try to catch object of monitor " + lock1);
-                synchronized (lock1) {
-                    System.out.println(Thread.currentThread().getName() + " monitor of object " + lock1 + " was caught");
-                    System.out.println(Thread.currentThread().getName() + " try to catch object of monitor  " + lock2);
-                    synchronized (lock2) {
-                        System.out.println(Thread.currentThread().getName() + " monitor of object " + lock2 + " was caught");
-                    }
-                }
-            }
-        }).start();
     }
 }
