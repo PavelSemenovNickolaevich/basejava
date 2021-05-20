156119c1dbb2c8e7953275874b63bc804a388262
Pavel Semenov <PNSemenov@sberbank.ru>
2021-05-20 07:32:26 +0300

    corrected Deadlock class

--- src/com/urise/webapp/DeadLock.java
+++ src/com/urise/webapp/DeadLock.java
@@ -11,23 +11,24 @@ class DeadLock {
     }
 
     public static void deadLock(Object lock1, Object lock2) {
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                System.out.println(Thread.currentThread().getName() + " try to catch object of monitor " + lock1);
-                synchronized (lock1) {
-                    System.out.println(Thread.currentThread().getName() + " monitor of object " + lock1 + " was caught");
-                    try {
-                        Thread.sleep(1000);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                    System.out.println(Thread.currentThread().getName() + " try to catch object of monitor  " + lock2);
-                    synchronized (lock2) {
-                        System.out.println(Thread.currentThread().getName() + " monitor of object " + lock2 + " was caught");
-                    }
+        new Thread(() -> {
+            System.out.println(getNameThread() + " try to catch object of monitor " + lock1);
+            synchronized (lock1) {
+                System.out.println(getNameThread() + " monitor of object " + lock1 + " was caught");
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println(getNameThread() + " try to catch object of monitor  " + lock2);
+                synchronized (lock2) {
+                    System.out.println(getNameThread() + " monitor of object " + lock2 + " was caught");
                 }
             }
         }).start();
+    }
+
+    private static String getNameThread() {
+        return Thread.currentThread().getName();
     }
 }
