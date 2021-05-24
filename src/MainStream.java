a36012f861187713ec07cd86f225014c3ab27ac0
Pavel Semenov <PNSemenov@sberbank.ru>
2021-05-24 22:18:40 +0300

    corrected HW12

--- src/MainStream.java
+++ src/MainStream.java
@@ -1,5 +1,3 @@
-package HW12;
-
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -7,31 +5,28 @@ import java.util.Scanner;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
-public class Task1 {
+public class MainStream {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Введите размер массива: ");
+        System.out.print("Please, enter array size: ");
         int size = scanner.nextInt();
         int[] arr = new int[size];
         for (int i = 0; i < size; i++) {
+            System.out.print("Enter number: ");
             arr[i] = scanner.nextInt();
         }
-        minValue(arr);
-        System.out.println("\n-----------------------");
-        List<Integer> list = new ArrayList<>();
-        list.add(2);
-        list.add(2);
-        list.add(3);
-        list.add(4);
-        list.add(1);
+        System.out.println(minValue(arr));
 
+        System.out.println("\n-----------------------");
+
+        Integer[] arr2 = {1, 2, 4, 5};
+      //  Integer[] arr2 = {8,9};
+        List<Integer> list = Arrays.asList(arr2);
         System.out.println(oddOrEven(list));
     }
 
     public static int minValue(int[] values) {
-        IntStream streamValues = Arrays.stream(values);
-        streamValues.distinct().sorted().forEach(System.out::print);
-        return 0;
+        return IntStream.of(values).distinct().sorted().reduce(0, (x,y) -> 10 * x + y);
     }
 
 //    реализовать метод List<Integer> oddOrEven(List<Integer> integers) если сумма всех чисел нечетная
@@ -39,7 +34,8 @@ public class Task1 {
 //    - решение в один стрим.
 
     public static List<Integer> oddOrEven(List<Integer> integers) {
-        int sum =  integers.stream().reduce((acc, element) ->  acc += element).get();
+       // int sum =  integers.stream().reduce((acc, element) ->  acc += element).get();
+        int sum =  integers.stream().reduce(0, Integer::sum);
         return integers.stream().filter( x -> (x % 2) == (sum % 2)).collect(Collectors.toList());
     }
 }
